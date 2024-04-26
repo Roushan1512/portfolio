c3ef21bc4c4bd6d63eab022f70926d489f4ad1fd
Roushan Poddar <roxie4269@gmail.com>
2024-04-26 22:51:35 +0530

    nav done

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,15 +1,28 @@
+import { Exo_2 } from "next/font/google";
 import React from "react";
+
+const garamond = Exo_2({ subsets: ["latin"] });
 
 const Home = () => {
   return (
-    <div id="home" className="h-[100vh] w-[100vw]">
+    <div
+      id="home"
+      className="h-[100vh] w-[100vw] flex flex-col justify-center items-center gap-16 relative"
+    >
       <img
-        src="/images/hexes.jpg"
+        src="/images/milky.jpg"
         alt=""
-        className="object-cover block absolute w-[100%] h-[100%] -z-10 brightness-[0.25]"
+        className="object-cover block absolute w-[100%] h-[100%] -z-10 brightness-[0.75]"
       />
-      <span className="h-12 w-[100vw]"></span>
-      <h1 className="text-white text-4xl font-bold text-center pt-16">HOME</h1>
+      <h1
+        className={`text-[80px] bg-clip-text text-transparent bg-gradient-to-r from-amber-200 via-orange-400 to-amber-200 font-medium text-center ${garamond.className}`}
+      >
+        Roushan Poddar
+      </h1>
+      {/* <span className="h-12" /> */}
+      <p className="text-white text-4xl font-semibold text-center">
+        Full Stack Developer
+      </p>
     </div>
   );
 };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, stagger } from "framer-motion";
 import Link from "next/link";
 import React from "react";
+import { Alegreya, Allura } from "next/font/google";
+
+const alegreya = Alegreya({ subsets: ["latin"] });
+const allura = Allura({ subsets: ["latin"], weight: "400" });
 
 const Nav = () => {
   const outerVariants = {
@@ -11,7 +15,7 @@ const Nav = () => {
     },
     visible: {
       opacity: 1,
-      transition: { staggerChildren: 0.3 },
+      transition: { staggerChildren: 0.3, delayChildren: 2 },
     },
   };
 
@@ -19,29 +23,120 @@ const Nav = () => {
     hidden: { y: -100 },
     visible: { y: 0 },
   };
+
+  const outerNameVariants = {
+    hidden: {
+      opacity: 1,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+        delayChildren: 0.5,
+        staggerDirection: -1,
+      },
+    },
+  };
+
+  const nameVariants = {
+    hidden: { x: -1000 },
+    visible: { x: 0 },
+  };
+
   return (
-    <motion.div
-      className="fixed top-0 flex justify-end items-center gap-10 h-fit py-2 px-16 w-[100%] z-10 bg-[#ffffff60]"
-      variants={outerVariants}
-      initial="hidden"
-      animate="visible"
-    >
-      <motion.span variants={innerVariants}>
-        <Link href="#home">Home</Link>
-      </motion.span>
-      <motion.span variants={innerVariants}>
-        <Link href="#skills">Skills</Link>
-      </motion.span>
-      <motion.span variants={innerVariants}>
-        <Link href="#projects">Projects</Link>
-      </motion.span>
-      <motion.span variants={innerVariants}>
-        <Link href="#about">About</Link>
-      </motion.span>
-      <motion.span variants={innerVariants}>
-        <Link href="#contact">Contact</Link>
-      </motion.span>
-    </motion.div>
+    <div className="fixed top-0 flex justify-center items-center pt-1 h-fit w-[100%] bg-white-500">
+      {/* Name Component  */}
+
+      <motion.div
+        className={`flex justify-start items-center h-fit py-2 px-16 w-[50%] ${allura.className} text-white text-5xl`}
+        variants={outerNameVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.span
+          variants={nameVariants}
+          transition={{ type: "spring", bounce: 0 }}
+        >
+          R
+        </motion.span>
+        <motion.span
+          variants={nameVariants}
+          transition={{ type: "spring", bounce: 0 }}
+        >
+          o
+        </motion.span>
+        <motion.span
+          variants={nameVariants}
+          transition={{ type: "spring", bounce: 0 }}
+        >
+          u
+        </motion.span>
+        <motion.span
+          variants={nameVariants}
+          transition={{ type: "spring", bounce: 0 }}
+        >
+          s
+        </motion.span>
+        <motion.span
+          variants={nameVariants}
+          transition={{ type: "spring", bounce: 0 }}
+        >
+          h
+        </motion.span>
+        <motion.span
+          variants={nameVariants}
+          transition={{ type: "spring", bounce: 0 }}
+        >
+          a
+        </motion.span>
+        <motion.span
+          variants={nameVariants}
+          transition={{ type: "spring", bounce: 0 }}
+        >
+          n
+        </motion.span>
+      </motion.div>
+
+      {/* Links Component  */}
+
+      <motion.div
+        className={`flex justify-end items-center gap-10 h-fit px-16 w-[50%] text-white text-lg font-semibold ${alegreya.className}`}
+        variants={outerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.span
+          variants={innerVariants}
+          transition={{ type: "spring", bounce: 0.25 }}
+        >
+          <Link href="#home">Home</Link>
+        </motion.span>
+        <motion.span
+          variants={innerVariants}
+          transition={{ type: "spring", bounce: 0.25 }}
+        >
+          <Link href="#skills">Skills</Link>
+        </motion.span>
+        <motion.span
+          variants={innerVariants}
+          transition={{ type: "spring", bounce: 0.25 }}
+        >
+          <Link href="#projects">Projects</Link>
+        </motion.span>
+        <motion.span
+          variants={innerVariants}
+          transition={{ type: "spring", bounce: 0.25 }}
+        >
+          <Link href="#about">About</Link>
+        </motion.span>
+        <motion.span
+          variants={innerVariants}
+          transition={{ type: "spring", bounce: 0.25 }}
+        >
+          <Link href="#contact">Contact</Link>
+        </motion.span>
+      </motion.div>
+    </div>
   );
 };
 
